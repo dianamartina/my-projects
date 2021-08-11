225df60231db744a64e3cdaaa2385a3c659d864d
dianamartina <dianamartina000@gmail.com>
2021-08-11 23:59:13 +0300

    update nav functionality mobile

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// --------- Closing menu ---------
+
+const allLinksNav = document.querySelectorAll(".main-nav-link");
+
+allLinksNav.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // console.log(e);
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({ top: 0, behaviour: "smooth" });
+
+    //Scroll to section
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (href !== "#" && !href.startsWith("#")) {
+      window.open(`${href}`, "_blank");
+    }
+
+    //Close mobile navigation
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
